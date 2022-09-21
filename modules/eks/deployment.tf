64747511fd3754aed71838c05b7e7474fbc228b2
Pandiyan <pandiec@gmail.com>
2022-09-21 21:50:02 +0530

    adsafdaf

--- modules/eks/deployment.tf
+++ modules/eks/deployment.tf
@@ -6,15 +6,15 @@ resource "kubernetes_namespace_v1" "amazon_cloudwatch" {
 }
 
 # Resource: kubectl_manifest which will create k8s Resources from the URL specified in above datasource
-# resource "kubectl_manifest" "fluentbit_resources" {
-#   depends_on = [
-#     kubernetes_namespace_v1.amazon_cloudwatch,
-#     kubernetes_config_map_v1.fluentbit_configmap,
-#     kubectl_manifest.cwagent_daemonset
-#     ]
-#   for_each = data.kubectl_file_documents.fluentbit_docs.manifests    
-#   yaml_body = each.value
-# }
+resource "kubectl_manifest" "fluentbit_resources" {
+  depends_on = [
+    kubernetes_namespace_v1.amazon_cloudwatch,
+    kubernetes_config_map_v1.fluentbit_configmap,
+    kubectl_manifest.cwagent_daemonset
+    ]
+  for_each = data.kubectl_file_documents.fluentbit_docs.manifests    
+  yaml_body = each.value
+}
 
 # Resource: MySQL Kubernetes Deployment
 resource "kubernetes_deployment_v1" "mysql_deployment" {
