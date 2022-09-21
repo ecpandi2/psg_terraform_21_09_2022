2ba2c45f9349a251c29dd7f78251fdbc67788b5d
Pandiyan <pandiec@gmail.com>
2022-09-21 20:07:35 +0530

    dafadsa

--- modules/eks/lb.tf
+++ modules/eks/lb.tf
@@ -93,19 +93,19 @@ resource "helm_release" "loadbalancer_controller" {
     
 }
 
-# Resource: Kubernetes Ingress Class
-resource "kubernetes_ingress_class_v1" "ingress_class_default" {
-  depends_on = [helm_release.loadbalancer_controller]
-  metadata {
-    name = "my-aws-ingress-class"
-    annotations = {
-      "ingressclass.kubernetes.io/is-default-class" = "true"
-    }
-  }  
-  spec {
-    controller = "ingress.k8s.aws/alb"
-  }
-}
+# # Resource: Kubernetes Ingress Class
+# resource "kubernetes_ingress_class_v1" "ingress_class_default" {
+#   depends_on = [helm_release.loadbalancer_controller]
+#   metadata {
+#     name = "my-aws-ingress-class"
+#     annotations = {
+#       "ingressclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }  
+#   spec {
+#     controller = "ingress.k8s.aws/alb"
+#   }
+# }
 
 ## Additional Note
 # 1. You can mark a particular IngressClass as the default for your cluster. 
