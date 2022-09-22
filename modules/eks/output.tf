95ce3c5b0e244242bb60bbbba0a41480a1c953f7
Pandiyan <pandiec@gmail.com>
2022-09-22 19:57:11 +0530

    Adding the Load Balancer

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -24,24 +24,24 @@ output "ebs_csi_helm_metadata" {
   value = helm_release.ebs_csi_driver.metadata
 }
 
-# output "lbc_iam_policy" {
-#   value = data.http.lbc_iam_policy.body
-# }
+output "lbc_iam_policy" {
+  value = data.http.lbc_iam_policy.body
+}
 
-# output "lbc_iam_policy_arn" {
-#   value = aws_iam_policy.lbc_iam_policy.arn 
-# }
+output "lbc_iam_policy_arn" {
+  value = aws_iam_policy.lbc_iam_policy.arn 
+}
 
-# output "lbc_iam_role_arn" {
-#   description = "AWS Load Balancer Controller IAM Role ARN"
-#   value = aws_iam_role.lbc_iam_role.arn
-# }
+output "lbc_iam_role_arn" {
+  description = "AWS Load Balancer Controller IAM Role ARN"
+  value = aws_iam_role.lbc_iam_role.arn
+}
 
-# # Helm Release Outputs
-# output "lbc_helm_metadata" {
-#   description = "Metadata Block outlining status of the deployed release."
-#   value = helm_release.loadbalancer_controller.metadata
-# }
+# Helm Release Outputs
+output "lbc_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value = helm_release.loadbalancer_controller.metadata
+}
 
 # output "externaldns_iam_policy_arn" {
 #   value = aws_iam_policy.externaldns_iam_policy.arn 
