87b228fb0d28a57d3dac7a58c74e9553162ccd26
Pandiyan <pandiec@gmail.com>
2022-09-22 20:28:23 +0530

    Add another certiificate

--- modules/eks/acm.tf
+++ modules/eks/acm.tf
@@ -1,6 +1,6 @@
 # Resource: ACM Certificate
 resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "*.cmcloudlab1845.info"
+  domain_name       = "*.cmcloudlab750.info"
   validation_method = "DNS"
 
   tags = {
